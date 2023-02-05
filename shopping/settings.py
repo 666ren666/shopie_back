3c5282627696071726af1d2c1c4a569f946ea863
ran at home <relitmy@gmail.com>
2023-02-05 17:51:53 +0200

    fiexd settings

--- shopping/settings.py
+++ shopping/settings.py
@@ -167,7 +167,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
     "http://localhost:8000",
-    "https://shopping-front.onrender.com",
+    "https://shopie_react.onrender.com",
     
 ]
 
