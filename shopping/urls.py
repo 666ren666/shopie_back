ba71eb7a2382df5b3d6fd46050e5852c33cfcdb9
ran at home <relitmy@gmail.com>
2023-02-05 16:52:51 +0200

    uplaod project to github

---
+++ shopping/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from users import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view()),
+    path('logout/', views.LogoutView.as_view()),
+    path('', include('product.urls')),
+]
